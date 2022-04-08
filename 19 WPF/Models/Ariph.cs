86bbbf2ec270b287af3efd50f2c28552ccd40c85
Никита Егоров <nik.egorov1994@mail.ru>
2022-04-08 18:34:50 +0300

    Добавьте файлы проекта.

---
+++ 19 WPF/Models/Ariph.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_WPF.Models
+{
+    internal class Ariph
+    {
+         public static double Add(double a) => 2 * a * Math.PI;
+    }
+}
---
+++ 19 WPF/VievModels/MainWindowVievModel.cs
@@ -0,0 +1,58 @@
+﻿using _19_WPF.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace _19_WPF.VievModels
+{
+    internal class MainWindowVievModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+            void OnPropertyChanged([CallerMemberName]string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+        private double number1;
+        public double Number1
+        {
+            get => number1;
+            set
+            {
+                number1=value;
+                OnPropertyChanged();
+            }
+        }
+        private double number2;
+        public double Number2
+        {
+            get => number2;
+            set
+            {
+                number2 = value;
+                OnPropertyChanged();
+            }
+        }
+        public ICommand AddCommand { get; }
+        private void OnAddCommandExecute (object p)
+        {
+            Number2 = Ariph.Add(Number1);
+        }
+        private bool CanAddCommandExecute(object p)
+        {
+            if (Number1 != 0)
+                return true;
+            else
+                return
+                false;
+        }
+        public MainWindowVievModel()
+        {
+            AddCommand = new RelayCommand(OnAddCommandExecute, CanAddCommandExecute);
+        }
+    }
+}
